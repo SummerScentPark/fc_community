cc587efefc0fe99ab40d609738c475cf85b1724b
Summerscent Park <summerscentpark@gmail.com>
2019-09-29 20:45:14 +0900

    Ch 4-6

--- fcuser/views.py
+++ fcuser/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
+from .forms import LoginForm
 
 # Create your views here.
 
@@ -24,24 +25,8 @@ def logout(request):
 
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        res_data = {}
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야 합니다.'
-        else:
-            fcuser = Fcuser.objects.get(username=username)
-            if check_password(password, fcuser.password):
-                request.session['user_pk'] = fcuser.id
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호 오류'
-
-    return render(request, 'login.html', res_data)
+    form = LoginForm()
+    return render(request, 'login.html', {'form': form})
 
 
 def register(request):
