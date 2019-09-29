6789dec195f6b7536a9131583197e4d579d59027
Summerscent Park <summerscentpark@gmail.com>
2019-09-29 17:14:04 +0900

    Ch 4-3

--- fcuser/urls.py
+++ fcuser/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from fcuser.views import home
 
 urlpatterns = [
     path('register/', views.register),
     path('login/', views.login),
+    path('', home),
 ]
