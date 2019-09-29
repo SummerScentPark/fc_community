d264ebd468dbe9c9f9ee1df3d619a31aaefe0693
Summerscent Park <summerscentpark@gmail.com>
2019-09-29 13:56:17 +0900

    Ch 2.8

--- fcuser/views.py
+++ fcuser/views.py
@@ -1,4 +1,3 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.hashers import make_password
 from .models import Fcuser
@@ -11,20 +10,22 @@ def register(request):
         return render(request, 'register.html')
     elif request.method == 'POST':
         username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
         password = request.POST.get('password', None)
         re_password = request.POST.get('re-password', None)
 
         res_data = {}
 
-        if not (username and password and re_password):
+        if not (username and useremail and password and re_password):
             res_data['error'] = '모든 값을 입력해야 합니다.'
 
         elif password != re_password:
             res_data['error'] = '비밀번호가 다릅니다.'
-            # return HttpResponse('비밀번호가 다릅니다')
+
         else:
             fcuser = Fcuser(
                 username=username,
+                useremail=useremail,
                 password=make_password(password)
             )
             fcuser.save()
